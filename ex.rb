7458754e18ebe10e75969329330632e9f3e0700e
Kathleen Thorpe <kathleenthorpe64@gmail.com>
2022-08-24 19:10:27 +0200

    added extra logic to output

---
+++ ex.rb
@@ -0,0 +1,12 @@
+{"coord"=>{"lon"=>2.159, "lat"=>41.3888},
+"weather"=>[{"id"=>801, "main"=>"Clouds", "description"=>"few clouds", "icon"=>"02d"}],
+"base"=>"stations",
+"main"=>{"temp"=>30.52, "feels_like"=>35.54, "temp_min"=>28.72, "temp_max"=>33.99, "pressure"=>1014, "humidity"=>67},
+"visibility"=>10000,
+"wind"=>{"speed"=>6.17,
+  "deg"=>210},
+  "clouds"=>{"all"=>20},
+  "dt"=>1661355921, "sys"=>{"type"=>2,
+    "id"=>48718, "country"=>"ES", "sunrise"=>1661317769, "sunset"=>1661366309},
+    "timezone"=>7200, "id"=>3128760,
+    "name"=>"Barcelona", "cod"=>200}
